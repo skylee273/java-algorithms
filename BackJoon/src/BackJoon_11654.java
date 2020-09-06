9fde2ddd28a271bf988f739a94aacb71dc962971
이하늘 <skylee273@koreatech.ac.kr>
2020-09-06 22:32:21 +0900

    string

--- BackJoon/src/BackJoon_11654.java
+++ BackJoon/src/BackJoon_11654.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class BackJoon_11654 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char name;
+		
+		Scanner sc = new Scanner(System.in);
+		name = sc.next().charAt(0);
+		int answer = (int) name;
+		System.out.println((answer));
 	}
 
 }
